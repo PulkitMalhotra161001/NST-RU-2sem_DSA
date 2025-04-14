cb0de03b82e3ce158e2c60406f435feda39a7e4d
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2025-04-14 11:29:14 +0530

    Update Balance Parenthesis - Easy Version.py

--- Stack/Balance Parenthesis - Easy Version.py
+++ Stack/Balance Parenthesis - Easy Version.py
@@ -18,3 +18,21 @@ else:
 
 
 ___________________________________________________
+
+
+def solve(s):
+    st = []
+    for i in s:
+        if i=='(':
+            st.append("(")
+        else:
+            if len(st)==0:
+                return "False"
+            st.pop()
+    
+    if len(st)==0:
+        return "True"
+    return "False"
+
+s=input()
+print(solve(s))
