b8e12ac43c5c182d6719ba39ec369d10f8de5ec8
Pulkit Malhotra <63710339+PulkitMalhotra161001@users.noreply.github.com>
2025-04-21 11:47:07 +0530

    Create Maximum Depth of Binary Tree.py

---
+++ Tree/Maximum Depth of Binary Tree.py
@@ -0,0 +1,8 @@
+def max_depth(root):
+    if root==None:
+        return 0
+    
+    left_height = max_depth(root.left)
+    right_height = max_depth(root.right)
+
+    return max(left_height,right_height) + 1
